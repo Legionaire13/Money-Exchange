52c57add0f05e2ffd5153d26e95213bd3161521d
legionaire13 <legionaire13@mail.ru>
2018-10-01 10:46:14 +0300

    basic conditions added

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if (currency >= 0) {
+        return {};
+    } else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    } 
+
 }
